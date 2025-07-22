949f3c4b62d26602583613025f87e0b571bda237
Devidas4011 <thombaredevidas511@gmail.com>
2025-07-22 21:04:05 +0530

    Add files via upload

---
+++ Assingment 1/qs8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+     int bs,id;
+     float hra=10,ba=30,tax=5,hm;
+     printf("enter the id:");
+     scanf("%d",&id);
+     printf("enter the basic salary ");
+     scanf("%d",&bs);
+    hra=(bs*hra/100);
+    ba=(bs*ba/100);
+    tax=(bs*tax/100);
+    hm=(hra-ba-tax);
+    printf("\nhouse rent:%.2f",hra);
+    printf("\nbasic dearness:%.2f",ba);
+    printf("\ntax:%.2f",tax);
+    printf("\nhome salary:%.2f",hm);
+    return 0;
+}
